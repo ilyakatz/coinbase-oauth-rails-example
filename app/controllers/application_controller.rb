ab896631fe2776617e9945f815372ff98810edbd
Ilya Katz <ilyakatz@gmail.com>
2014-08-09 16:47:27 -0400

    Re-authentication when Coinbase session times out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from OAuth2::Error do |exception|
+
+    if exception.code == "invalid_request"
+      sign_out(:user)
+      authenticate_user!
+    end
+
+  end
+
 end
